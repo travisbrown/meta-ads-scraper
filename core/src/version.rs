a1e2ac6e882a7fb7a6109e428c1659b44de74bdf
Travis Brown <travisrobertbrown@gmail.com>
2026-03-04 10:09:23 +0100

    Simply parsing and add tests

--- core/src/version.rs
+++ core/src/version.rs
@@ -29,18 +29,116 @@ impl FromStr for GraphApiVersion {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts = s.split('.').collect::<Vec<_>>();
+        let error = || Error::InvalidGraphApiVersion(s.to_string());
 
-        if parts.len() == 2 {
-            let (major, minor) = parts[0]
-                .parse()
-                .ok()
-                .zip(parts[1].parse().ok())
-                .ok_or_else(|| Error::InvalidGraphApiVersion(s.to_string()))?;
+        let (major, minor) = s
+            .split_once('.')
+            .filter(|(_, minor)| !minor.contains('.'))
+            .and_then(|(major_str, minor_str)| major_str.parse().ok().zip(minor_str.parse().ok()))
+            .ok_or_else(error)?;
 
-            Ok(Self { major, minor })
-        } else {
-            Err(Error::InvalidGraphApiVersion(s.to_string()))
-        }
+        Ok(Self { major, minor })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_typical_version() {
+        assert_eq!(
+            "24.0".parse::<GraphApiVersion>().unwrap(),
+            GraphApiVersion::new(24, 0)
+        );
+    }
+
+    #[test]
+    fn parse_non_zero_minor() {
+        assert_eq!(
+            "21.3".parse::<GraphApiVersion>().unwrap(),
+            GraphApiVersion::new(21, 3)
+        );
+    }
+
+    #[test]
+    fn parse_zero_zero() {
+        assert_eq!(
+            "0.0".parse::<GraphApiVersion>().unwrap(),
+            GraphApiVersion::new(0, 0)
+        );
+    }
+
+    #[test]
+    fn parse_large_values() {
+        assert_eq!(
+            "65535.65535".parse::<GraphApiVersion>().unwrap(),
+            GraphApiVersion::new(u16::MAX, u16::MAX),
+        );
+    }
+
+    #[test]
+    fn parse_missing_dot() {
+        assert!("240".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_three_part_version() {
+        assert!("24.0.1".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_empty_string() {
+        assert!("".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_non_numeric_major() {
+        assert!("vX.0".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_non_numeric_minor() {
+        assert!("24.x".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_overflow_major() {
+        assert!("65536.0".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_overflow_minor() {
+        assert!("0.65536".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_leading_dot() {
+        assert!(".0".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn parse_trailing_dot() {
+        assert!("24.".parse::<GraphApiVersion>().is_err());
+    }
+
+    #[test]
+    fn error_message_contains_input() {
+        let err = "bad".parse::<GraphApiVersion>().unwrap_err();
+        assert!(
+            err.to_string().contains("bad")
+                || matches!(err, Error::InvalidGraphApiVersion(s) if s == "bad")
+        );
+    }
+
+    #[test]
+    fn display_roundtrip() {
+        let v = GraphApiVersion::new(24, 0);
+        assert_eq!(v.to_string().parse::<GraphApiVersion>().unwrap(), v);
+    }
+
+    #[test]
+    fn display_format() {
+        assert_eq!(GraphApiVersion::new(21, 3).to_string(), "21.3");
     }
 }
